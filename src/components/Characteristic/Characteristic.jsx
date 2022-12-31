2dbb67b10bbb92763275596682f46c31068285d4
Kolya-7 <111074234+Kolya-7@users.noreply.github.com>
2022-12-31 22:19:39 +0500

    add

--- src/components/Characteristic/Characteristic.jsx
+++ src/components/Characteristic/Characteristic.jsx
@@ -9,7 +9,7 @@ function Characteristic() {
                 <li className="functions__point">Экран: <span className="bold-text">6.1</span></li>
                 <li className="functions__point">Встроенная память: <span className="bold-text">128 ГБ</span></li>
                 <li className="functions__point">Операционная система: <span className="bold-text">iOS 15</span></li>
-                <li className="functions__point">Беспроводные интерфейсы: <span className="bold-text">NFC,Bluetooth, <span className="unfo-1"> Wi-Fi</span></span></li>
+                <li className="functions__point">Беспроводные интерфейсы: <span className="bold-text">NFC,Bluetooth, Wi-Fi</span></li>
                 <li className="functions__point">Процессор: <a href="https://ru.wikipedia.org/wiki/Apple_A15" target="_blank" rel="noopener noreferrer">Apple A15 Bionic</a></li>
                 <li className="functions__point">Вес: <span className="bold-text">173 г</span></li>
             </ul>  
